5e63a8d7d0fe9840bd30e6abbd6d67c53b2d271a
Yanelis Lopez <yane3628@gmail.com>
2017-02-20 04:07:32 -0500

    Fix error where not printing receive

--- Client.java
+++ Client.java
@@ -21,59 +21,67 @@ public class Client {
 
         String ipAddress = args[0];
         int port = Integer.parseInt(args[1]);
-        Socket server = new Socket(ipAddress, port);
-        String message = null;
-        String userInput = null;
-
         try {
-            PrintWriter outWriter = new PrintWriter(server.getOutputStream(), true);
-            BufferedReader inBuffer = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            Socket server = new Socket(ipAddress, port);
 
-            // Reads "Hello!" from server
-            message = inBuffer.readLine();
-            printReceive(message);
+            String message = null;
+            String userInput = null;
 
-            Scanner scanner = new Scanner(System.in);
+            try {
+                PrintWriter outWriter = new PrintWriter(server.getOutputStream(), true);
+                BufferedReader inBuffer = new BufferedReader(new InputStreamReader(server.getInputStream()));
 
-            // Loop that is constantly waiting for user input
-            while(true) {
-                try {
-                    userInput = scanner.nextLine();
-                    outWriter.println(userInput);
+                // Reads "Hello!" from server
+                message = inBuffer.readLine();
+                printReceive(message);
 
-                    // Message received from server
-                    message = inBuffer.readLine();
+                Scanner scanner = new Scanner(System.in);
 
-                    switch (Integer.parseInt(message)) {
-                        case -1:
-                            printReceive("incorrect operation command.");
-                            break;
-                        case -2:
-                            printReceive("number of inputs is less than two.");
-                            break;
-                        case -3:
-                            printReceive("number of inputs is more than four.");
-                            break;
-                        case -4:
-                            printReceive("one or more of the inputs contain(s) non-number(s).");
-                            break;
-                        case -5:
-                            printReceive("exit.");
-                            server.close();
-                            System.exit(0);
-                            break;
-                        default:
-                            break;
+                // Loop that is constantly waiting for user input
+                while(true) {
+                    try {
+                        userInput = scanner.nextLine();
+                        outWriter.println(userInput);
+
+                        // Message received from server
+                        message = inBuffer.readLine();
+
+                        switch (Integer.parseInt(message)) {
+                            case -1:
+                                printReceive("incorrect operation command.");
+                                break;
+                            case -2:
+                                printReceive("number of inputs is less than two.");
+                                break;
+                            case -3:
+                                printReceive("number of inputs is more than four.");
+                                break;
+                            case -4:
+                                printReceive("one or more of the inputs contain(s) non-number(s).");
+                                break;
+                            case -5:
+                                printReceive("exit.");
+                                server.close();
+                                System.exit(0);
+                                break;
+                            default:
+                                printReceive(message);
+                                break;
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Server not responding. Please restart server and then restart client.");
+                        server.close();
+                        System.exit(-1);
                     }
-                } catch (Exception e) {
-                    server.close();
-                    System.exit(-1);
                 }
+            } catch (IOException e) {
+                System.out.println("Could not create BufferedReader and/or PrintWriter");
+                server.close();
+                System.exit(-1);
             }
-        } catch (IOException e) {
-            System.out.println("Could not create BufferedReader and/or PrintWriter");
-            server.close();
-            System.exit(-1);
+        } catch(Exception e) {
+            System.out.println("Server is not running. Please start server.");
+            System.exit(0);
         }
     }
 }
