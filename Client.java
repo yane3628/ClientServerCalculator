10510de671ea8427eea1c841aa55261827c31553
Yanelis Lopez <yane3628@gmail.com>
2017-02-20 02:54:46 -0500

    Add comments and finish execption handling

--- Client.java
+++ Client.java
@@ -6,9 +6,13 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
+    // Helper method to print in the "receive: " format
+    // parameters:
+    //  message - String to print with "receive: " appended
     public static void printReceive(String message) {
         System.out.println("receive: " + message);
     }
+
     public static void main(String[] args) throws IOException {
         String ipAddress = args[0];
         int port = Integer.parseInt(args[1]);
@@ -16,8 +20,14 @@ public class Client {
         String message = null;
         String userInput = null;
 
-        PrintWriter outWriter = new PrintWriter(server.getOutputStream(), true);
-        BufferedReader inBuffer = new BufferedReader(new InputStreamReader(server.getInputStream()));
+        try {
+            PrintWriter outWriter = new PrintWriter(server.getOutputStream(), true);
+            BufferedReader inBuffer = new BufferedReader(new InputStreamReader(server.getInputStream()));
+        } catch (IOException e) {
+            System.out.println("Could not create BufferedReader and/or PrintWriter");
+            server.close();
+            System.exit(-1);
+        }
 
         // Reads "Hello!" from server
         message = inBuffer.readLine();
@@ -25,36 +35,40 @@ public class Client {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Loop that is constantly waiting for user input
         while(true) {
-            userInput = scanner.nextLine();
-            outWriter.println(userInput);
+            try {
+                userInput = scanner.nextLine();
+                outWriter.println(userInput);
 
-            message = inBuffer.readLine();
+                // Message received from server
+                message = inBuffer.readLine();
 
-            switch (Integer.parseInt(message)) {
-                case -1:
-                    printReceive("incorrect operation command.");
-                    break;
-                case -2:
-                    printReceive("number of inputs is less than two.");
-                    break;
-                case -3:
-                    printReceive("number of inputs is more than four.");
-                    break;
-                case -4:
-                    printReceive("one or more of the inputs contain(s) non-number(s).");
-                    break;
-                case -5:
-                    printReceive("exit.");
-                    server.close();
-                    System.exit(0);
-                    break;
-                default:
-                    printReceive(message);
-                    break;
+                switch (Integer.parseInt(message)) {
+                    case -1:
+                        printReceive("incorrect operation command.");
+                        break;
+                    case -2:
+                        printReceive("number of inputs is less than two.");
+                        break;
+                    case -3:
+                        printReceive("number of inputs is more than four.");
+                        break;
+                    case -4:
+                        printReceive("one or more of the inputs contain(s) non-number(s).");
+                        break;
+                    case -5:
+                        printReceive("exit.");
+                        server.close();
+                        System.exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            } catch (Exception e) {
+                server.close();
+                System.exit(-1);
             }
-
         }
-        // System.exit(0);
     }
 }
--- Server.java
+++ Server.java
@@ -6,20 +6,28 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    // Error codes
     private static int incorrectOperationCommand = -1;
     private static int numInputsLTTwo = -2;
     private static int numInputsGTFour = -3;
     private static int nan = -4;
     private static int exitCode = -5;
 
+    // Acceptable operations
     public enum Operation {
         add, subtract, multiply
     }
 
+    // Helper method to print in the "get: " format
+    // parameters:
+    //  message - String to print with "get: " appended
     public static void printGet(String message) {
         System.out.print("get: " + message);
     }
 
+    // Helper method to print in the ", return: " format
+    // parameters:
+    //  message - String to print with ", return: " appended
     public static void printReturn(String message) {
         System.out.println(", return: " + message);
     }
@@ -39,140 +47,134 @@ public class Server {
             server = new ServerSocket(serverPort);
 
             try {
-                // Exit with 'terminate'
+                // Loop that waits for a client
                 while(true) {
                     try {
+                        // Establish connection with client
                         client = server.accept();
-                        // TODO make sure this address is right when running on two IP addresses
                         System.out.println("get connection from ... " + client.getInetAddress().getHostAddress());
 
                         try {
                             inBuffer = new BufferedReader(new InputStreamReader(client.getInputStream()));
                             outWriter = new PrintWriter(client.getOutputStream(), true);
                         } catch (IOException e) {
-                            System.out.println("Read failed 1");
+                            System.out.println("Could not create BufferedReader and/or PrintWriter");
+                            client.close();
                             System.exit(-1);
                         }
 
+                        // Send "Hello!" to client
                         outWriter.println("Hello!");
 
-                        // Exit with 'bye'
+                        // Loop that constantly reads from client
                         while(true) {
-                            // try{
-                                message = inBuffer.readLine();
-                                printGet(message);
+                            message = inBuffer.readLine();
+                            printGet(message);
 
-                                String[] messageArray = message.split("\\s+");
+                            String[] messageArray = message.split("\\s+");
 
-                                if (message.equalsIgnoreCase("bye")) {
-                                    outWriter.println(exitCode);
-                                    printReturn(Integer.toString(exitCode));
+                            if (message.equalsIgnoreCase("bye")) {
+                                outWriter.println(exitCode);
+                                printReturn(Integer.toString(exitCode));
 
-                                    // Close current client socket connection
-                                    client.close();
-                                }
-                                else if(message.equalsIgnoreCase("terminate")) {
-                                    outWriter.println(exitCode);
-                                    printReturn(Integer.toString(exitCode));
+                                // Close current client socket connection
+                                client.close();
+                            }
+                            else if(message.equalsIgnoreCase("terminate")) {
+                                outWriter.println(exitCode);
+                                printReturn(Integer.toString(exitCode));
 
-                                    // Close current client socket connection
-                                    client.close();
-                                    server.close();
-                                    System.exit(0);
-                                }
-                                else {
-                                    boolean noErrors = true;
-                                    // Check if add, subtract, or multiply
-                                    try {
-                                        Operation operation = Operation.valueOf(messageArray[0]);
+                                // Close current client and server sockets and terminate
+                                client.close();
+                                server.close();
+                                System.exit(0);
+                            }
+                            else {
+                                boolean noErrors = true;
+                                // Check if add, subtract, or multiply
+                                try {
+                                    Operation operation = Operation.valueOf(messageArray[0]);
 
-                                        // Check number of inputs
-                                        if(messageArray.length < 3) {
-                                            // Send error code -2 for number of inputs is less than two
-                                            outWriter.println(numInputsLTTwo);
-                                            printReturn(Integer.toString(numInputsLTTwo));
-                                        }
-                                        else if(messageArray.length > 5) {
-                                            // Send error code -3 for number of inputs is more than 4
-                                            outWriter.println(numInputsGTFour);
-                                            printReturn(Integer.toString(numInputsGTFour));
-                                        }
-                                        else {
-                                            // Check everything is an int (positive)
-                                            for(int i = 1; i < messageArray.length; i++) {
-                                                if(!messageArray[i].matches("\\d+")) {
-                                                    // Send error code -4 for inputs contains non-numbers
-                                                    outWriter.println(nan);
-                                                    printReturn(Integer.toString(nan));
-
-                                                    noErrors = false;
-                                                    break;
-                                                }
-                                            }
-
-                                            if(noErrors) {
-                                                switch (operation) {
-                                                    case add:
-                                                        int sum = 0;
-                                                        for(int i = 1; i < messageArray.length; i++) {
-                                                            sum += Integer.parseInt(messageArray[i]);
-                                                        }
-
-                                                        outWriter.println(sum);
-                                                        printReturn(Integer.toString(sum));
-                                                        break;
-                                                    case subtract:
-                                                        int sub = Integer.parseInt(messageArray[1]);
-                                                        for(int i = 2; i < messageArray.length; i++) {
-                                                            sub -= Integer.parseInt(messageArray[i]);
-                                                        }
-
-                                                        outWriter.println(sub);
-                                                        printReturn(Integer.toString(sub));
-                                                        break;
-                                                    case multiply:
-                                                        int mult = Integer.parseInt(messageArray[1]);
-                                                        for(int i = 2; i < messageArray.length; i++) {
-                                                            mult *= Integer.parseInt(messageArray[i]);
-                                                        }
-
-                                                        outWriter.println(mult);
-                                                        printReturn(Integer.toString(mult));
-                                                        break;
-                                                    default:
-                                                        outWriter.println("0");
-                                                        printReturn(Integer.toString(0));
-                                                        break;
-                                                }
-                                            }
-                                        }
-                                    } catch(IllegalArgumentException e) {
-                                        // Send error code -1 for incorrect operation command
-                                        outWriter.println(incorrectOperationCommand);
-                                        printReturn(Integer.toString(incorrectOperationCommand));
+                                    // Check number of inputs
+                                    if(messageArray.length < 3) {
+                                        // Send error code -2 for number of inputs is less than two
+                                        outWriter.println(numInputsLTTwo);
+                                        printReturn(Integer.toString(numInputsLTTwo));
                                     }
+                                    else if(messageArray.length > 5) {
+                                        // Send error code -3 for number of inputs is more than 4
+                                        outWriter.println(numInputsGTFour);
+                                        printReturn(Integer.toString(numInputsGTFour));
+                                    }
+                                    else {
+                                        // Check everything is an int
+                                        for(int i = 1; i < messageArray.length; i++) {
+                                            if(!messageArray[i].matches("\\d+")) {
+                                                // Send error code -4 for inputs contains non-numbers
+                                                outWriter.println(nan);
+                                                printReturn(Integer.toString(nan));
 
+                                                noErrors = false;
+                                                break;
+                                            }
+                                        }
+
+                                        if(noErrors) {
+                                            switch (operation) {
+                                                case add:
+                                                    int sum = 0;
+                                                    for(int i = 1; i < messageArray.length; i++) {
+                                                        sum += Integer.parseInt(messageArray[i]);
+                                                    }
+
+                                                    outWriter.println(sum);
+                                                    printReturn(Integer.toString(sum));
+                                                    break;
+                                                case subtract:
+                                                    int sub = Integer.parseInt(messageArray[1]);
+                                                    for(int i = 2; i < messageArray.length; i++) {
+                                                        sub -= Integer.parseInt(messageArray[i]);
+                                                    }
+
+                                                    outWriter.println(sub);
+                                                    printReturn(Integer.toString(sub));
+                                                    break;
+                                                case multiply:
+                                                    int mult = Integer.parseInt(messageArray[1]);
+                                                    for(int i = 2; i < messageArray.length; i++) {
+                                                        mult *= Integer.parseInt(messageArray[i]);
+                                                    }
+
+                                                    outWriter.println(mult);
+                                                    printReturn(Integer.toString(mult));
+                                                    break;
+                                                default:
+                                                    break;
+                                            }
+                                        }
+                                    }
+                                } catch(IllegalArgumentException e) {
+                                    // Send error code -1 for incorrect operation command
+                                    outWriter.println(incorrectOperationCommand);
+                                    printReturn(Integer.toString(incorrectOperationCommand));
                                 }
-                                //Send data back to client
-                                // outWriter.println(message);
-                            // } catch (IOException e) {
-                            //     System.out.println("Read failed 2");
-                            //     System.exit(-1);
-                            // }
-                            // Deconstruct inBuffer and outWriter
+                            }
                         }
                     } catch(Exception e) {
-                    //     System.out.println("Couldn't accept client");
-                    }
 
+                    } finally {
+                        // Ensure client is closed
+                        client.close();
+                    }
                 }
-            } finally { // Change to catch
+            } finally {
+                // Ensure server is closed
                 server.close();
             }
         }
         catch(IOException e) {
-            System.out.println("Connection could not be established");
-            System.exit(-5);
+            System.out.println("ServerSocket could not be made");
+            System.exit(-1);
         }
     }
 }
